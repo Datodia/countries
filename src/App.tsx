e3abd6fcac3124b1527349d465bedb97fb1d1b4e
Datodia <dato.diasamidze.02@gmail.com>
2022-12-26 22:31:33 +0400

    feat: add countires

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import styled from 'styled-components'
+import axios from 'axios'
+import { countries } from './Interfaces'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState<countries[]>([])
+
+  // const fetchData = () => {
+  //   axios.get('https://restcountries.com/v3.1/all')
+  //     .then(res => {
+  //       setData(res.data)
+  //     })
+  // }
+
+  useEffect(() => {
+    axios.get('https://restcountries.com/v2/all')
+      .then(res => {
+        setData(res.data)
+      })
+  }, [])
 
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-      <Square />
-    </div>
+    <Wrapper>
+      {/* <Card>
+        <img />
+        <h4>name</h4>
+        <h6>Population</h6>
+        <h6>region</h6>
+        <h6>capital</h6>
+      </Card> */}
+
+
+      {data.map((item) => {
+        return (<Card>
+          <img src={item.flags.svg} />
+          <h4>{item.name}</h4>
+          <h6>{item.capital}</h6>
+          <h6>{item.population}</h6>
+          <h6>{item.region}</h6>
+        </Card>)
+      })}
+
+    </Wrapper>
   )
 }
 
@@ -20,3 +54,11 @@ const Square = styled.div`
   height: 50px;
   background-color: red;
 `
+const Wrapper = styled.div`
+  flex-wrap: wrap;
+`
+
+const Card = styled.div`
+  border: 2px solid red;
+`
+
---
+++ src/Interfaces.ts
@@ -0,0 +1,10 @@
+export interface countries {
+    name: string;
+    capital: string;
+    flags: {
+        svg: string
+    };
+    population: number;
+    region: string;
+
+}
